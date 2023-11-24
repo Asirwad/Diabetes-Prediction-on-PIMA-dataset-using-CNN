aa0c10ccb720f8e23f864ffe3d763af601bf0db9
Asirwad <asirwadsali@gmail.com>
2023-11-24 10:16:26 +0530

    init

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import StandardScaler
 import numpy as np
+from joblib import load
 
 model = load_model('models/model.h5')
 
@@ -15,15 +16,14 @@ DiabetesPedigreeFunction = float(input("Enter the Diabetes Pedigree Function: ")
 Age = float(input("Enter the Age: "))
 
 # Prepare input for prediction
-scaler = StandardScaler()
+scaler = load('models/scalar.joblib')
 input_values = np.array([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
-input_values = scaler.fit_transform(input_values)
+input_values = scaler.transform(input_values)
 input_values = input_values.reshape(1, input_values.shape[1], 1)
 
-
 prediction = model.predict(input_values)
-print(prediction[0][0])
-if prediction[0][0] >= 0.5:
+print(prediction)
+if prediction >= 0.5:
     print("The patient is predicted to have diabetes.")
 else:
     print("The patient is predicted not to have diabetes.")
--- app/train.py
+++ app/train.py
@@ -1,7 +1,7 @@
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from joblib import dump
 
 from app.arch import get_model
 
@@ -18,7 +18,8 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 # Standardize the data
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
-x_test = scaler.fit_transform(x_test)
+x_test = scaler.transform(x_test)
+dump(scaler, 'models/scalar.joblib')
 
 # Reshape the data for CNN (add a channel dimension)
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1], 1)
@@ -26,5 +27,5 @@ x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], 1)
 
 model = get_model(input_shape=(x_train.shape[1], 1))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(x_train, y_train, epochs=150, batch_size=32, validation_data=(x_test, y_test))
+model.fit(x_train, y_train, epochs=35, batch_size=32, validation_data=(x_test, y_test))
 model.save("models/model.h5")
